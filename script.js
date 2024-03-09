244425a03928097117775b8f8e742ea32c90d3df
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-09 13:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+let form=document.getElementById('userForm');
+form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const age = parseInt(document.getElementById('age').value);
+        const name = document.getElementById('name').value;
+
+        if (!age || !name) {
+            alert("Please fill in all fields.");
+            return;
+        }
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (age >= 18) {
+                    resolve(`Welcome, ${name}. You can vote.`);
+                } else {
+                    reject(`Oh sorry ${name}. You aren't old enough.`);
+                }
+            }, 4000);
+        });
+        promise.then((message) => {
+            alert(message);
+        }).catch((error) => {
+            alert(error);
+        });
+    });
